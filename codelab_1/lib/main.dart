86413d37f1f0c9d059648e077f0eabd629a219fb
CHARMIN-PC\Oscar David <o.d.anota@gmail.com>
2018-03-22 14:25:27 -0600

    whit animation and themes

--- codelab_1/lib/main.dart
+++ codelab_1/lib/main.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';  // For using customized theme
+import 'package:flutter/cupertino.dart';
+
 
 void main(){
   runApp( new FriendlychatApp()  );
 }
+
+//Customized theme for IOS
+final ThemeData kIOSTheme = new ThemeData(
+  primarySwatch: Colors.orange,
+  primaryColor: Colors.grey[100],
+  primaryColorBrightness: Brightness.light,
+);
+
+//Customized theme for Android
+final ThemeData kDefaultTheme = new ThemeData(
+  primarySwatch: Colors.purple,
+  accentColor: Colors.orangeAccent[400],
+);
 
 class FriendlychatApp extends StatelessWidget{
   @override
@@ -10,6 +26,9 @@ class FriendlychatApp extends StatelessWidget{
     // TODO: implement build
     return new MaterialApp(
       title: "FriendlyChat",
+      theme: defaultTargetPlatform == TargetPlatform.iOS
+          ? kIOSTheme
+          : kDefaultTheme,
       home: new ChatScreen(), //home:= Default screen or the main UI for app
     );
   }
@@ -20,15 +39,18 @@ class ChatScreen extends StatefulWidget {
   State createState() => new ChatScreenState();
 }
 
-class ChatScreenState extends State<ChatScreen>{
+class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin{
   final List<ChatMessage> _messages = <ChatMessage>[];
   final TextEditingController _textController = new TextEditingController(); // TO manage interactions with the textField
+  bool _isComposing = false;
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return new Scaffold( //Scaffold := Implements the basic material design visual layout structure.
       appBar: new AppBar( //AppBar:= is a horizontal bar typically shown at the top of an app using the appBar property.
-        title: new Text("FriendlyChat")),
+        title: new Text("FriendlyChat"),
+        elevation: Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
+      ),
       body: new Column(
         children: <Widget>[
           new Flexible(
@@ -51,6 +73,13 @@ class ChatScreenState extends State<ChatScreen>{
     );
   }
 
+  @override
+  void dispose() {
+    for (ChatMessage message in _messages)
+      message.animationController.dispose();
+    super.dispose();
+  }
+
   Widget _buildTextComposer(){
     return new IconTheme(
         data: new IconThemeData(color: Theme.of(context).accentColor),
@@ -60,6 +89,11 @@ class ChatScreenState extends State<ChatScreen>{
             children: <Widget>[new Flexible(
               child: new TextField(
                 controller: _textController,
+                onChanged: (String text){
+                  setState((){
+                    _isComposing = text.length > 0;
+                  });
+                },
                 onSubmitted: _handleSubmitted, //onSubmitted: To be notified when the user submits a message
                 decoration: new InputDecoration.collapsed(
                   hintText: "Send a message"),
@@ -69,7 +103,9 @@ class ChatScreenState extends State<ChatScreen>{
               margin: new EdgeInsets.symmetric(horizontal: 4.0),
               child: new IconButton(
                 icon: new Icon(Icons.send),
-                onPressed: () => _handleSubmitted(_textController.text), // => expression is shorthand for { return expression; }
+                onPressed: _isComposing
+                  ? () => _handleSubmitted(_textController.text) // => expression is shorthand for { return expression; }
+                  : null,
               ),
              ),
             ],
@@ -80,40 +116,59 @@ class ChatScreenState extends State<ChatScreen>{
 
   void _handleSubmitted(String text) {
     _textController.clear();
-    ChatMessage message = new ChatMessage(text: text,);
+    setState(() {
+      _isComposing = false;
+    });
+    ChatMessage message = new ChatMessage(
+        text: text,
+        animationController: new AnimationController(
+          duration: new Duration(milliseconds: 700),
+          vsync: this,
+        ),
+    );
     setState((){ //You call setState()to modify _messages and to let the framework know this part of the widget tree has changed and it needs to rebuild the UI.
       _messages.insert(0, message);
     });
+    message.animationController.forward();
   }
 }
 const String _name = "David";
 
 class ChatMessage extends StatelessWidget {
-  ChatMessage({this.text});
+  ChatMessage({this.text, this.animationController});
   final String text;
+  final AnimationController animationController;
+
   @override
   Widget build(BuildContext context) {
-    return new Container(
-      margin: const EdgeInsets.symmetric(vertical: 10.0),
-      child: new Row(
-        crossAxisAlignment: CrossAxisAlignment.start, //gives it the highest position along the vertical axis.
-        children: <Widget>[
-          new Container(
-            margin: const EdgeInsets.only(right: 16.0),
-            child: new CircleAvatar(child: new Text(_name[0])), //For the avatar, the parent is a Row widget whose main axis is horizontal
+    return new SizeTransition(
+        sizeFactor: new CurvedAnimation(
+          parent: animationController, curve: Curves.easeOut),
+          axisAlignment: 0.0,
+          child: new Container(
+            margin: const EdgeInsets.symmetric(vertical: 10.0),
+            child: new Row(
+              crossAxisAlignment: CrossAxisAlignment.start, //gives it the highest position along the vertical axis.
+              children: <Widget>[
+                new Container(
+                  margin: const EdgeInsets.only(right: 16.0),
+                  child: new CircleAvatar(child: new Text(_name[0])), //For the avatar, the parent is a Row widget whose main axis is horizontal
+                ),
+                new Expanded(
+                  child: new Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[ // For messages, the parent is a Column widget whose main axis is vertical, so CrossAxisAlignment.start aligns the text at the furthest left position along the horizontal axis.
+                      new Text(_name, style: Theme.of(context).textTheme.subhead),
+                      new Container(
+                        margin: const EdgeInsets.only(top: 5.0),
+                        child: new Text(text),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
-          new Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[ // For messages, the parent is a Column widget whose main axis is vertical, so CrossAxisAlignment.start aligns the text at the furthest left position along the horizontal axis.
-              new Text(_name, style: Theme.of(context).textTheme.subhead),
-              new Container(
-                margin: const EdgeInsets.only(top: 5.0),
-                child: new Text(text),
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
+        );
   }
 }
