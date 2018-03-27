e651f3d0219bfab98740d8d2f5b4380313a8eb71
CHARMIN-PC\Oscar David <o.d.anota@gmail.com>
2018-03-27 12:18:09 -0600

    Template for creating StatefulWidget classes

---
+++ Plantillas de clases/EsqueletoStatefulWidget.dart
@@ -0,0 +1,13 @@
+class YellowBird extends StatefulWidget {
+  const YellowBird({ Key key }) : super(key: key);
+
+  @override
+  _YellowBirdState createState() => new _YellowBirdState();
+}
+
+class _YellowBirdState extends State<YellowBird> {
+  @override
+  Widget build(BuildContext context) {
+    return new Container(color: const Color(0xFFFFE306));
+  }
+}
